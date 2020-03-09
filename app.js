871f531170f1f3227fd68237a0a7c7a626e9e80e
Robert Moel <robert.moel@rvmconsulting1.com>
2020-03-09 19:44:33 -0400

    made classes, inquirer, addMember, addManager work

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const util = require("util");
 const inq = require("inquirer");
-const mngr = require("./lib/manager");
-const engr = require("./lib/engineer");
-const intr = require("./lib/intern");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 function concatJSON(json1, json1){
     return JSON.stringify(JSON.parse(json1).concat(JSON.parse(json2)));
@@ -10,6 +11,7 @@ function concatJSON(json1, json1){
 
 var teamList = [];
 
+// this are all for use with the "when" keyword
 const validStr = async (inName) =>{
     if(inName.length===0){
         return "Name cannot be blank"
@@ -52,68 +54,134 @@ const doneCheck = async(ans) => {
     else return true
 }
 
-async function addMembers(){
-    ans2 = await inq
-    .prompt([
-        {
-            name:"teamMemType",
-            message: "Please select a team member type",
-            default: "Engineer",
-            type: "list",
-            choices: ["Manager", "Engineer", "Intern", "Done"]
-        },
-        {
-            name:"name",
-            when: doneCheck,
-            message: "Please enter your name",
-            validate: validStr
-        },
-        {
-            name:"id",
-            when: doneCheck,
-            type:"number",
-            message:"Please enter an ID number",
-            validate: validNum
-        },
-        {
-            name: "email",
-            when: doneCheck,
-            message: "Please enter a valid email address",
-            validate: validEmail
-        },
-        {
-            name:"github",
-            when: engrCheck,
-            type:"input",
-            message:"Please enter your github username",
-            validate: validStr
-        },
-        {
-            name: "officenum",
-            when: mngrCheck,
-            type:"number",
-            message: "Please enter your office number",
-            validate: validNum
-        },
-        {
-            name:"school",
-            when:intrCheck,
-            message: "Please enter your school",
-            validate: validStr
+const QManager = [{
+    name:"name", 
+    when: doneCheck,
+    type:"input",
+    message: "Please enter Manager's name",
+    validate: validStr
+},
+{
+    name:"id",
+    when: doneCheck,
+    type:"number",
+    message:"Please enter Manager ID number",
+    validate: validNum
+},
+{
+    name: "email",
+    when: doneCheck,
+    message: "Please enter a valid email address",
+    validate: validEmail
+},
+{
+    name: "officenum",
+    type:"number",
+    message: "Please enter your office number",
+}]
+
+const QMembers = [{
+    name:"teamMemType",
+    message: "Please select a team member type",
+    default: "Engineer",
+    type: "list",
+    choices: ["Engineer", "Intern"]
+},
+{
+    name:"name", 
+    type:"input",
+    message: "Please enter your name",
+    validate: validStr
+},
+{
+    name:"id",
+    type:"number",
+    message:"Please enter an ID number",
+    validate: validNum
+},
+{
+    name: "email",
+    message: "Please enter a valid email address",
+    validate: validEmail
+},
+{
+    name:"github",
+    when: engrCheck,
+    type:"input",
+    message:"Please enter your github username",
+    validate: validStr
+},
+{
+    name:"school",
+    when:intrCheck,
+    message: "Please enter your school",
+    validate: validStr
+}]
+
+const QDone = 
+[
+    {
+        name:"done",
+        message: "Add another team member?",
+        default: "Y",
+        type:"confirm"
+    }
+]
+
+function AddMember(){
+inq
+    .prompt(QMembers).then(function(ans2){
+        switch(ans2.teamMemType){
+            case "Engineer":
+                engr = new Engineer(ans2.name, ans2.id, ans2.email, ans2.github);
+                teamList.push(engr);
+                break;
+            case "Intern":
+                intr = new Intern(ans2.name, ans2.id, ans2.email, ans2.school);
+                teamList.push(intr);
+                break;
         }
-    ])
-    if(ans2.teamMemType !== "Done"){
-        teamList.push(ans2);
-        console.log("teamList:");
-        console.log(teamList)
-        addMembers();
-    } else {
-        console.log("at function return");
+
         console.log(teamList);
-        return teamList;
+        MakeChoice();
+    });
+}
+
+function AddManager(){
+    inq
+    .prompt(QManager)
+    .then(function(ans3){
+        console.log(ans3);
+        mgr = new Manager(ans3.name, ans3.id, ans3.email,ans3.officenum);
+        teamList.push(mgr);
+        MakeChoice();
+    })
+}
+
+function MakeChoice(){
+    inq
+    .prompt(QDone)
+    .then(function(ans4){
+        if(!ans4.done){
+            BuildHTML();
+        } else MakeTeam();
+    });
+};
+
+addMgr = false;
+function MakeTeam(){
+    if(!addMgr) {
+        addMgr = true;
+        AddManager();
+    }
+    else {
+        AddMember();
     }
 }
-    out = addMembers();
-    console.log(out);
-    console.log("After return");
+
+function BuildHTML(){
+    console.log("Building HTML");
     console.log(teamList);
+}
+
+MakeTeam();
